1503cdef88c42f8c42ee196bfdbf4b095732f58d
dhzin <victorc.k006@gmail.com>
2021-04-07 19:27:34 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5594991731479 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO DHZIN 🐊🚩 :*
+_https: https://chat.whatsapp.com/LA3c55AHrGNH5ClWuShO6c'
+}
+exports.daftarvip = daftarvip
